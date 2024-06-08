f5f450cafe0249c466a715c175bc60d4517ecb1d
yur-f <yurf3z0@gmail.com>
2024-06-09 00:40:29 +0300

    test CI8

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,22 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+env_secret_path = os.path.join(
+    os.path.join(os.path.dirname(os.getcwd())), ".env.secret"
+)
+
+if not os.path.exists(env_secret_path):
+    with open(env_secret_path, "w") as f:
+        pass  # Создаем пустой файл
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=["../.env.dev", "../src/.env.secret"],
+        env_file=[
+            os.path.join(os.path.join(os.path.dirname(os.getcwd()), ".env.dev")),
+            os.path.join(os.path.join(os.path.dirname(os.getcwd()), ".env.secret")),
+        ],
         env_file_encoding="utf-8",
     )
     host: str
