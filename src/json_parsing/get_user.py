7fb30f3766c553467fc07a69d2c29d984ba6ede6
yur-f <yurf3z0@gmail.com>
2024-06-09 01:05:16 +0300

    test CI11

--- src/json_parsing/get_user.py
+++ src/json_parsing/get_user.py
@@ -17,12 +17,16 @@ def get_users_url(count_users: int, settings: Settings) -> Union[List[Dict], boo
     :return: dict - если удалось получить json файл с сайта | bool - если не удалось получить json
     """
     try:
-        with requests.get(settings.url + str(count_users)) as response:
-            if response.status_code == 200:
-                data: Dict = response.json()
-                return data["results"]
-            else:
-                return False
+        if settings.url is None:
+            log.error("URL для подключения отсутствует в настройках.")
+            return False
+        else:
+            with requests.get(settings.url + str(count_users)) as response:
+                if response.status_code == 200:
+                    data: Dict = response.json()
+                    return data["results"]
+                else:
+                    return False
     except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as e:
         log.error(f"Ошибка: {e}")
         return False
