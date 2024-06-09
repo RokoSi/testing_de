2ca41c3c82fa22b19344e5e191b57a38704ba110
yur-f <yurf3z0@gmail.com>
2024-06-09 18:03:05 +0300

    test CI19

--- src/validators/validator_password.py
+++ src/validators/validator_password.py
@@ -7,12 +7,9 @@ def validator_pass(password: str) -> bool:
     :param password: Пароль для валидации.
     :return: True - если пароль валиден, False - если пароль не валиден.
     """
-    if (
+    return (
         any(char in string.punctuation for char in password)
         and any(char in string.digits for char in password)
         and any(char in string.ascii_uppercase for char in password)
         and any(char in string.ascii_lowercase for char in password)
-    ):
-        return True
-    else:
-        return False
+    )
